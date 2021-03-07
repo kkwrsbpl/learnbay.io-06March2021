3eb1af68ab7dec63dd8e7b0dd41bc9c1675adab5
Karmveer Kumar <kkwrsbpl@gmail.com>
2021-03-07 13:58:38 +0530

    Update LinkList.cpp
    added new function for removal of link list and find circular

--- LinkList.cpp
+++ LinkList.cpp
@@ -93,13 +93,137 @@ namespace LinkList
          }
          else
          {
-            Node* next = curreNode->nextNode;
+            Node* nextNode = curreNode->nextNode;
             Node* tempNode = new Node(dataToInsert);
             curreNode->nextNode = tempNode;
-            tempNode->nextNode = next;
-
+            tempNode->nextNode = nextNode;
          }
       }
+
+      void deleteNodeFromStart()
+      {
+         if (startNode == nullptr)
+         {
+            cout << "No node available to delete" << endl;
+         }
+         else
+         {
+            Node* currentNode = startNode;
+            startNode = currentNode->nextNode;
+            delete[] currentNode;
+         }
+      }
+
+      void deleteNodeFromLast()
+      {
+         if (startNode == nullptr)
+         {
+            cout << "No node available to delete" << endl;
+         }
+         else
+         {
+            Node* currentNode = startNode;
+            while (currentNode->nextNode->nextNode != nullptr)
+            {
+               currentNode = currentNode->nextNode;
+            }
+            delete[] currentNode->nextNode->nextNode;
+            currentNode->nextNode = nullptr;
+         }
+      }
+
+      void deleteNodeAfter(int existingNodeData)
+      {
+         if (startNode == nullptr)
+         {
+            cout << "No node available to delete" << endl;
+         }
+         else
+         {
+            Node* curreNode = startNode;
+            Node* tempNode = nullptr;
+            while (nullptr != curreNode->nextNode && curreNode->data != existingNodeData)
+            {
+               curreNode = curreNode->nextNode;
+            }
+            tempNode = curreNode->nextNode;
+            curreNode->nextNode = curreNode->nextNode->nextNode;
+            delete[] tempNode;
+            tempNode = nullptr;
+         }
+      }
+
+      void deleteNodeBefore(int existingNodeData)
+      {
+         Node* curreNode = startNode;
+         Node* prevNode = startNode;
+         while (nullptr != curreNode->nextNode && curreNode->nextNode->data != existingNodeData)
+         {
+            prevNode = curreNode;
+            curreNode = curreNode->nextNode;
+         }
+         if (curreNode == nullptr)
+         {
+            cout << existingNodeData << " " << "doesn't exist" << endl;
+         }
+         else
+         {
+            prevNode->nextNode = curreNode->nextNode;
+            delete[] curreNode;
+            curreNode = nullptr;
+         }
+      }
+
+      void reverseLikList()
+      {
+         Node* curreNode = startNode;
+         Node* nextNode = nullptr;
+         Node* PreviousNode = nullptr;
+         while (curreNode != nullptr)
+         {
+            nextNode = curreNode->nextNode;
+            curreNode->nextNode = PreviousNode;
+            PreviousNode = curreNode;
+            curreNode = nextNode;
+         }
+         startNode = PreviousNode;
+      }
+
+      Node* getLastNode()
+      {
+         Node* currentNode = startNode;
+         while (currentNode->nextNode != nullptr)
+         {
+            currentNode = currentNode->nextNode;
+         }
+         return currentNode;
+      }
+
+      Node* getStartNode()
+      {
+         return startNode;
+      }
+
+      bool LoopExist()
+      {
+         Node* slowNode = startNode;
+         Node* fastNode = startNode;
+         bool loopExist = false;
+         while (slowNode != nullptr && fastNode != nullptr && fastNode->nextNode != nullptr)
+         {
+            slowNode = slowNode->nextNode;
+            fastNode = fastNode->nextNode->nextNode;
+            if (slowNode == fastNode)
+            {
+               loopExist = true;
+               break;
+            }
+         }
+         return loopExist;
+      }
+
+
+
 
       void printList()
       {
@@ -126,6 +250,55 @@ int main()
    list->addNodeAfter(100, 10);
 
    list->printList();
+
+   list->deleteNodeFromStart();
+
+   cout << endl;
+
+   list->printList();
+
+   list->deleteNodeFromLast();
+
+   cout << endl;
+
+   list->printList();
+
+   cout << "delete node after 20 " << endl;
+   list->deleteNodeAfter(20);
+
+   cout << endl;
+
+   list->printList();
+
+   cout << "delete node before 35 " << endl;
+   list->deleteNodeBefore(35);
+
+   cout << endl;
+
+   list->printList();
+
+   cout << endl;
+
+   list->reverseLikList();
+
+   cout << endl;
+
+   list->printList();
+
+   cout << endl;
+
+   
+   //LinkList::Node *LastNode = list->getLastNode();
+   //LastNode->nextNode = list->getStartNode();
+
+   cout << endl;
+
+   list->printList();
+
+   cout<<"loop exist - "<<list->LoopExist();
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -138,4 +311,3 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
